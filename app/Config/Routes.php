4deeb1b62e6c02799957765a7d4a1402c6b73f52
RANDRIANTSEHENO Tokiniaina <r.tokiniaina@outlook.com>
2026-07-20 08:52:02 +0300

    routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,22 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->addRedirect('/', '/login');
-$routes->get('/login', 'UserController::login');
-$routes->post('/login', 'UserController::postLogin');
-$routes->get('/logout', 'UserController::logout');
-$routes->get('/test', 'TestController::index');
+$routes->get('/login', 'ClientController::login');
+$routes->post('/login', 'ClientController::postLogin');
+$routes->get('/logout', 'ClientController::logout');
+
+$routes->group('client', function ($routes) {
+    $routes->get('/operations', 'ClientController::operations');
+    $routes->get('/operations/new', 'ClientController::operationsNew');
+    $routes->post('/operations/new', 'ClientController::newOperation');
+});
+
+$routes->group('operateur', function ($routes) {
+    $routes->get('/login', 'OperateurController::login');
+    $routes->post('/login', 'OperateurController::postLogin');
+    $routes->get('/dashboard', 'OperateurController::dashboard');
+    $routes->get('/prefixes', 'OperateurController::prefixes');
+    $routes->post('/prefixes/edit', 'OperateurController::prefixesEdit');
+    $routes->get('/frais', 'OperateurController::frais');
+    $routes->post('/frais/edit', 'OperateurController::fraisEdit');
+});
