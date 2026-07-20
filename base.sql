d88fff3066052c891977908bdf7d9b4a52487055
RANDRIANTSEHENO Tokiniaina <r.tokiniaina@outlook.com>
2026-07-20 08:24:43 +0300

    base de données

---
+++ base.sql
@@ -0,0 +1,32 @@
+prefixes:
+	id
+	valeur
+
+types_operations:
+	id
+	libelle
+
+baremes_frais:
+	id
+	type
+	montant_min
+	montant_max
+	frais
+
+clients:
+	id
+	numero
+
+operations:
+	id
+	type
+	montant
+	frais
+	num_source
+	num_destination
+	date_operation
+
+admins:
+	id
+	email
+	mot_de_passe
